b5d251e68e17da5409c4629883cafd51073126ab
summerliu <summer.ycliu@gmail.com>
2020-02-07 23:51:16 +0800

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// callback function for HMR
+function initialApplication() {
+    // use commonJS require here because webpack HMR can't handle ES6 import
+    const App = require('./pages/App').default;
+    // render the app and save the new root element
+    ReactDOM.render(<App/>, document.getElementById('root'));
+}
+
+initialApplication();
+
+// If this is webpack-dev-server, set up HMR
+if (module.hot) {module.hot.accept('./pages/App', initialApplication);}
---
+++ webpack.config.js
@@ -0,0 +1,155 @@
+const path = require('path');
+const webpack = require('webpack');
+const DotEnvPlugin = require('dotenv-webpack');
+const ExtractCssChunks = require('extract-css-chunks-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
+
+// const VERBOSE = process.argv.indexOf('--verbose') !== -1;
+const DEV_MODE = (process.env.NODE_ENV === 'development');
+const SRC_BASE_PATH = path.resolve(__dirname, './src');
+const OUTPUT_PATH = path.resolve(__dirname, './dist');
+
+// const cssLoaderScssOptions = {
+//     sourceMap: DEV_MODE,
+//     // CSS Modules https://github.com/css-modules/css-modules
+//     modules: false,
+//     localIdentName: 'l-[local]-[hash:base64:6]',
+//     importLoaders: 2,
+// };
+
+// const sassLoaderOptions = {
+//     sourceMap: DEV_MODE,
+// };
+
+const config = {
+    target: 'web',
+    entry: {
+        app: ['whatwg-fetch', 'url-polyfill', path.resolve(SRC_BASE_PATH, 'index.jsx')],
+    },
+
+    // where to dump the output of a production build
+    output: {
+        path: path.resolve(OUTPUT_PATH, 'assets'),
+        publicPath: '/assets/',
+        filename: '[name].[contenthash].js',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.jsx?/i,
+                loader: 'babel-loader',
+                exclude: /(node_modules|bower_components)/,
+                options: {
+                    presets: [
+                        '@babel/preset-env',
+                        '@babel/preset-react'
+                    ],
+                    plugins: [
+                        ['@babel/plugin-syntax-jsx'],
+                        ['@babel/plugin-transform-react-jsx'],
+                        ['@babel/plugin-transform-react-display-name'],
+                    ],
+                },
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        name: 'images/[name].[hash].[ext]',
+                        limit: 1024,
+                    },
+                }],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+        ],
+    },
+
+    resolve: {
+        extensions: ['.jsx', '.js', '.json'],
+        alias: {
+            'react': 'react',
+            'react-dom': 'react-dom',
+        },
+    },
+
+    plugins: [
+        new DotEnvPlugin({
+            safe: true,
+        }),
+        new ExtractCssChunks({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: '[name].[contenthash].css',
+            chunkFilename: '[name].[contenthash].css',
+            // hot: true, // if you want HMR - we try to automatically inject hot reloading but if it's not working, add it to the config
+            orderWarning: true, // Disable to remove warnings about conflicting order between imports
+            reloadAll: true, // when desperation kicks in - this is a brute force HMR flag
+            cssModules: true, // if you use cssModules, this can help.
+        }),
+        new CleanWebpackPlugin({
+            verbose: true,
+            // cleanAfterEveryBuildPatterns: ['static*.*', '!static1.js'],
+        }),
+        // [
+        //     path.join(OUTPUT_PATH, '*.html'),
+        //     path.join(OUTPUT_PATH, 'assets'),
+        // ], 
+        new HtmlWebpackPlugin({
+            template: path.resolve(SRC_BASE_PATH, 'index.html'),
+            filename: path.resolve(OUTPUT_PATH, 'index.html'),
+            favicon: path.resolve(SRC_BASE_PATH, 'components', 'assets', 'favicon.png'),
+            title: '5 x Ruby',
+            alwaysWriteToDisk: true, // only on when HtmlWebpackHarddiskPlugin is installed
+            inject: 'body',
+        }),
+        new HtmlWebpackHarddiskPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+
+    devServer: {
+        // serve up any static files from dist/
+        contentBase: path.join(__dirname, 'dist'),
+        // enable gzip compression:
+        compress: true,
+        // enable HMR
+        hot: true,
+        // watch the files served by the devServer.contentBase option.
+        watchContentBase: false,
+        host: '0.0.0.0',
+    },
+};
+
+module.exports = (env, argv) => {
+    if (argv.mode === 'development') {
+        config.mode = 'development';
+        config.devtool = 'source-map';
+        config.output.filename = '[name].js?[hash]';
+        config.output.chunkFilename = '[id].js?[hash]';
+    }
+
+    if (argv.mode === 'production') {
+        config.mode = 'production';
+        config.output.filename = '[name].[hash].js';
+        config.output.chunkFilename = '[id].[hash].js';
+    }
+    return config;
+};
