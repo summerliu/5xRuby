e803f7d8ff4044f8993e4886b5cb33640eccaf42
summerliu <summer.ycliu@gmail.com>
2020-02-11 10:47:42 +0800

    Change webpack output path

--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,8 @@ const config = {
 
   // where to dump the output of a production build
   output: {
-    path: path.resolve(OUTPUT_PATH, 'build'),
-    publicPath: '/build/',
+    path: path.resolve(OUTPUT_PATH),
+    publicPath: '/',
     filename: '[name].[contenthash].js',
   },
 
